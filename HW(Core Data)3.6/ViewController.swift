1f1bc6a635b4e659674ace2221cd3504fe792f08
Isa199510 <116788871+Isa199510@users.noreply.github.com>
2022-12-06 15:12:34 +0600

    Initial Commit

---
+++ HW(Core Data)3.6/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW(Core Data)3.6
+//
+//  Created by Иса on 06.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
